4a84a36f5870b63c9d88a88e4031c5d62bac5091
FrederikHolst2001 <145931650+FrederikHolst2001@users.noreply.github.com>
2026-02-02 18:06:46 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,3 @@
-// ---- Base44 compatibility stub (DO NOT REMOVE) ----
-window.base44 = {
-  get: async () => ({}),
-  invoke: async () => ({}),
-  me: async () => null,
-};
-// -----------------------------------------------
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
