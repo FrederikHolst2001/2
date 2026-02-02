f65f63658d329b3724ebadaf03786ddd818cfc27
FrederikHolst2001 <145931650+FrederikHolst2001@users.noreply.github.com>
2026-02-02 16:20:19 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ window.base44 = {
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '@/App.jsx'
-import '@/index.css'
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
