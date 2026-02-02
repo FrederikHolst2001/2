45704a8117ca6869156146812a6ac1f456fe485b
FrederikHolst2001 <145931650+FrederikHolst2001@users.noreply.github.com>
2026-02-02 16:19:11 +0100

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+window.base44 = {
+  get: async () => ({}),
+};
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from '@/App.jsx'
+import '@/index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+)
